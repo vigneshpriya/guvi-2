7509d71a4eb7188d75fc8bfac8e0224ba927efb0
priyasakthivel65 <51690693+priyasakthivel65@users.noreply.github.com>
2019-06-12 12:02:09 +0530

    Create vowel.py

---
+++ codekatta/vowel.py
@@ -0,0 +1,10 @@
+f=input()
+i=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+o=['a','e','i','o','u']
+if(f in i):
+  if(f in o):
+    print('Vowel')
+  else:
+    print('Consonant')
+else:
+  print('invalid')
